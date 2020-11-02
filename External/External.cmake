e8dcc330d4148208095add62069dd161d9af5bb3
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2020-11-02 19:10:35 +0100

    Update

--- External/External.cmake
+++ External/External.cmake
@@ -2,5 +2,5 @@ project(External)
 
 get_filename_component(KubeExternalDir ${CMAKE_CURRENT_LIST_FILE} PATH)
 
-add_library(ParallelHashmap INTERFACE)
-target_include_directories(ParallelHashmap INTERFACE ${KubeExternalDir}/parallel-hashmap)
+add_library(VulkanMemoryAllocator INTERFACE)
+target_include_directories(VulkanMemoryAllocator INTERFACE ${KubeExternalDir}/VulkanMemoryAllocator/src)
